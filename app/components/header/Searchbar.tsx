5798ea38af5ab057f384a49f0b39e72f6b0712d6
mhmd <ms.mhmdsalah@gmail.com>
2024-04-14 22:35:35 +0200

    in progress

--- app/components/header/Searchbar.tsx
+++ app/components/header/Searchbar.tsx
@@ -1,15 +1,38 @@
-import {SearchIcon} from "@heroicons/react/solid" 
+"use client";
+import { SearchIcon } from "@heroicons/react/solid";
+import { useState } from "react";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from "react-date-range";
+
 function Searchbar () {
+  const [input, setInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndtDate] = useState(new Date())
+  const selectionRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+  };
   return (
-    <div className="flex items-center md:border-2 rounded-full py-2 ">
-      <input
-        type="text"
-        placeholder="Search"
-        className="text-sm text-gray-600 placeholder-gray-400 flex-grow pl-5 bg-transparent outline-none"
-      />
-      <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
-    </div>
-  )
+    <>
+      <div className="flex items-center md:border-2 rounded-full py-2 ">
+        <input
+          type="text"
+          placeholder="Search"
+          className="text-sm text-gray-600 placeholder-gray-400 flex-grow pl-5 bg-transparent outline-none"
+          value={ input }
+          onChange={ e => { setInput(e.target.value); } }
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
+      { input && <div className=" absolute bg-white p-3 top-[100%] left-[50%] flex flex-col col-span-3 mx-auto translate-x-[-50%] ">
+        <DateRangePicker
+          ranges={ [selectionRange] }
+        />
+      </div> }
+    </>
+  );
 }
 
-export default Searchbar
+export default Searchbar;
