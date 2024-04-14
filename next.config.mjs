79765cda33ce4c710ba163e5f592ce912f46ae57
mhmd <ms.mhmdsalah@gmail.com>
2024-04-14 02:26:16 +0200

    in progress

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains:["images.unsplash.com","links.papareact.com"]
+  },
+  
+};
 
 export default nextConfig;
