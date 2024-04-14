be9fb0b9e4f1f5b879b4c76126c378dbb4514112
mhmd <ms.mhmdsalah@gmail.com>
2024-04-14 16:59:04 +0200

    in progress

--- app/components/Greats.tsx
+++ app/components/Greats.tsx
@@ -1,9 +1,36 @@
-function Greats() {
+import Image from "next/image";
+import Link from "next/link";
+
+type CreateProps = {
+  img: string,
+  title: string,
+  description: string,
+  linkText: string;
+};
+function Greats (props: CreateProps) {
   return (
-    <div>
-      
+    <div className="container relative my-10">
+      <div className=" h-96 w-full">
+        <Image
+          src={ props.img }
+          alt="this img"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-2xl -z-10"
+        />
+      </div>
+      <div className="absolute top-32 left-12">
+        <h3 className="text-4xl mb-3 w-64">{ props.title }</h3>
+        <p>{ props.description }</p>
+        <Link
+          className='text-sm px-4 py-2 rounded-lg mt-5 block text-white  w-fit bg-gray-900'
+          href={"/"}
+        >
+          { props.linkText }
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Greats
+export default Greats;
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,12 @@ export default function Home() {
       <Banner/>
       <Explore/>
       <Live/>
-      <Greats/>
+      <Greats
+        img='https://images.unsplash.com/photo-1609688669309-fc15db557633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        title="The Greatest Outdoors"
+        description="Wishlists curated by Airbnb"
+        linkText="Get Inspired"
+      />
     </main>
   );
 }
